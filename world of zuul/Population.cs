bfb677f7eea81b3c0e04bc7b29ba570ffe2c23dc
petar spirovski <spir4e2014@gmail.com>
2023-11-06 11:01:03 +0100

    Testing errors.

---
+++ world of zuul/Population.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WorldOfZuul
+{
+    public class Population
+    {
+        public int currentPopulation { get; private set; }
+
+        public Population(int initialPopulation)
+        {
+            currentPopulation = initialPopulation;
+        }
+
+        public Population()
+        {
+        }
+
+        public void IncreasePopulation(int amount)
+        {
+            currentPopulation += amount;
+        }
+
+        public void DecreasePopulation(int amount)
+        {
+            currentPopulation -= amount;
+        }
+    }
+}
